0e364a1da5013b73197263c336f7d1f81039ced2
arsalanhub <arsalanmohd237@gmail.com>
2023-03-04 13:35:47 +0530

    Styling & code formatting

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import Chip from '@mui/material/Chip';
+import Chip from "@mui/material/Chip";
 import BarChart from "./barChart";
 
 function App() {
@@ -34,19 +34,17 @@ function App() {
   };
 
   const resetOptions = async () => {
-    let tmpData = await axios.get(
-      "http://localhost:5000/getTodo?month=&year="
-    );
-    setYear([])
-    setMonth([])
-    setQueryMonth("")
-    setQueryYear("")
+    let tmpData = await axios.get("http://localhost:5000/getTodo?month=&year=");
+    setYear([]);
+    setMonth([]);
+    setQueryMonth("");
+    setQueryYear("");
     let tmpYear = await axios.get("http://localhost:5000/getYear");
     let tmpMonth = await axios.get("http://localhost:5000/getMonth");
     setData(tmpData.data.data);
     setYear(tmpYear.data.data[0].distinctYear);
     setMonth(tmpMonth.data.data[0].distinctMonth);
-  }
+  };
 
   const clickHander = async () => {
     await axios.post("http://localhost:5000/postTodo", {
@@ -71,128 +69,179 @@ function App() {
       `http://localhost:5000/getTodo?month=${month}&year=${queryYear}`
     );
     setData(tmpData.data.data);
-  }
+  };
 
   const updateHandler = async (id) => {
     let data = await axios.post(`http://localhost:5000/updateTodo/${id}`, {
-      text
-    })
-    setEditId(0)
-    fun()
-  }
+      text,
+    });
+    setEditId(0);
+    fun();
+  };
 
   const DragHandler = async () => {
-    let tmp=data[itemToBeDragged]
-    data[itemToBeDragged]=data[itemOnWhichDrag]
-    data[itemOnWhichDrag]=tmp;
-    console.log(itemOnWhichDrag, itemToBeDragged)
-    await axios.post(`http://localhost:5000/updateTodo/${data[itemToBeDragged]._id}`, {
-      index: itemToBeDragged
-    })
-    await axios.post(`http://localhost:5000/updateTodo/${data[itemOnWhichDrag]._id}`, {
-      index: itemOnWhichDrag
-    })
+    let tmp = data[itemToBeDragged];
+    data[itemToBeDragged] = data[itemOnWhichDrag];
+    data[itemOnWhichDrag] = tmp;
+    console.log(itemOnWhichDrag, itemToBeDragged);
+    await axios.post(
+      `http://localhost:5000/updateTodo/${data[itemToBeDragged]._id}`,
+      {
+        index: itemToBeDragged,
+      }
+    );
+    await axios.post(
+      `http://localhost:5000/updateTodo/${data[itemOnWhichDrag]._id}`,
+      {
+        index: itemOnWhichDrag,
+      }
+    );
     setItemOnWhichDrag(-1);
     setItemToBeDragged(-1);
-    fun()
-  }
+    fun();
+  };
 
   const checkBoxHandler = async (id, striked) => {
-    striked=!striked;
+    striked = !striked;
     await axios.post(`http://localhost:5000/updateTodo/${id}`, {
-        striked
-    })
-    fun()
-  }
+      striked,
+    });
+    fun();
+  };
 
   const deleteFun = async () => {
-    await axios.post(`http://localhost:5000/deleteTodo/${deleteId}`)
+    await axios.post(`http://localhost:5000/deleteTodo/${deleteId}`);
     setDeleteId(0);
-  }
+  };
 
   useEffect(() => {
     fun();
   }, []);
 
   useEffect(() => {
-    if(deleteId !== 0) {
-      deleteFun()
-      fun()
+    if (deleteId !== 0) {
+      deleteFun();
+      fun();
     }
-  }, [deleteId])
+  }, [deleteId]);
 
   return (
     <div style={{ display: "flex", marginTop: "3rem", marginRight: "1rem" }}>
       <div style={{ width: "45%" }}>
-      <Box sx={{ minWidth: 275 }} className="card-wrapper">
-          <Card variant="outlined">
+        <Box sx={{ minWidth: 275 }} className="card-wrapper">
+          <Card variant="outlined" style={{ backgroundColor: "aqua" }}>
             <CardContent style={{ display: "flex", padding: "1rem" }}>
-              <Typography variant="h5" component="div" style={{ display: "flex", width: "100%", justifyContent: "space-around"}}>
-                  Filters: 
-                  <select onChange={(e) => yearHandler(e.target.value)}>
-                    <option value="Select Year" selected>
-                      Select Year
-                    </option>
-                    {year &&
-                      year.map((ele, idx) => {
-                        return <option key={idx}>{ele}</option>;
-                      })}
-                  </select>
-                  <select onChange={(e) => monthHandler(e.target.value)}>
-                    <option value="Select Month" selected>
-                      Select Month
-                    </option>
-                    {month &&
-                      month.map((ele, idx) => {
-                        return (<option key={idx+10}>{ele}</option>)
-                      })}
-                  </select>
-                  <button onClick={()=> resetOptions()}>Clear</button>
+              <Typography
+                variant="h5"
+                component="div"
+                style={{
+                  display: "flex",
+                  width: "100%",
+                  justifyContent: "space-around",
+                }}
+              >
+                Filters:
+                <select onChange={(e) => yearHandler(e.target.value)}>
+                  <option value="Select Year" selected>
+                    Select Year
+                  </option>
+                  {year &&
+                    year.map((ele, idx) => {
+                      return <option key={idx}>{ele}</option>;
+                    })}
+                </select>
+                <select onChange={(e) => monthHandler(e.target.value)}>
+                  <option value="Select Month" selected>
+                    Select Month
+                  </option>
+                  {month &&
+                    month.map((ele, idx) => {
+                      return <option key={idx + 10}>{ele}</option>;
+                    })}
+                </select>
+                <button onClick={() => resetOptions()}>Clear</button>
               </Typography>
             </CardContent>
           </Card>
         </Box>
-      {data &&
-        data.map((ele, idx) => {
-          if (editId === ele._id)
-            return (
-              <div className="editBox">
-                <input type="text" onChange={(e)=> setText(e.target.value)} />
-                <button onClick={()=>updateHandler(ele._id)}>Save</button>
-              </div>
-            );
-          return (
-            <Box sx={{ minWidth: 275 }} className="card-wrapper">
-              <Card variant="outlined" draggable={true} 
-                      key={idx}
-                      onDragStart={() => setItemToBeDragged(idx)} 
-                      onDragEnter={() => setItemOnWhichDrag(idx)} 
-                      onDragEnd={() => DragHandler()} 
-                      onDragOver={(e) => e.preventDefault()}>
-                <CardContent style={{ display: "flex"}}>
-                  <Typography variant="h5" component="div">
-                    <input type="checkbox" checked={ele.striked} onClick={() => checkBoxHandler(ele._id, ele.striked)}/>
-                  </Typography>
-                  <Typography variant="h5" component="div" style={{ textDecoration: ele.striked ? "line-through" : "", display: "flex", width: "100%", justifyContent: "space-between" }}>
-                    <div style={{width: "53%"}}>{ele.text}</div>
-                    <Chip label={ele.date.split('T')[0]} />
-                     <div>
-                       <button onClick={()=> setEditId(ele._id)}>Edit</button>
-                       <button onClick={()=> setDeleteId(ele._id)}>Delete</button>
-                     </div>
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Box>
-          );
-        })}
+        <div style={{ maxHeight: "37rem", overflow: "auto" }}>
+          {data &&
+            data.map((ele, idx) => {
+              if (editId === ele._id)
+                return (
+                  <div className="editBox">
+                    <input
+                      type="text"
+                      onChange={(e) => setText(e.target.value)}
+                    />
+                    <button onClick={() => updateHandler(ele._id)}>Save</button>
+                  </div>
+                );
+              return (
+                <Box sx={{ minWidth: 275 }} className="card-wrapper">
+                  <Card
+                    variant="outlined"
+                    draggable={true}
+                    key={idx}
+                    onDragStart={() => setItemToBeDragged(idx)}
+                    onDragEnter={() => setItemOnWhichDrag(idx)}
+                    onDragEnd={() => DragHandler()}
+                    onDragOver={(e) => e.preventDefault()}
+                  >
+                    <CardContent style={{ display: "flex" }}>
+                      <Typography variant="h5" component="div">
+                        <input
+                          type="checkbox"
+                          checked={ele.striked}
+                          onClick={() => checkBoxHandler(ele._id, ele.striked)}
+                        />
+                      </Typography>
+                      <Typography
+                        variant="h5"
+                        component="div"
+                        style={{
+                          textDecoration: ele.striked ? "line-through" : "",
+                          display: "flex",
+                          width: "100%",
+                          justifyContent: "space-between",
+                        }}
+                      >
+                        <div style={{ width: "53%" }}>{ele.text}</div>
+                        <Chip label={ele.date.split("T")[0]} />
+                        <div>
+                          <button onClick={() => setEditId(ele._id)}>
+                            Edit
+                          </button>
+                          <button onClick={() => setDeleteId(ele._id)}>
+                            Delete
+                          </button>
+                        </div>
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </Box>
+              );
+            })}
+        </div>
         <Box sx={{ minWidth: 275 }} className="card-wrapper">
-          <Card variant="outlined">
+          <Card variant="outlined" style={{ backgroundColor: "aqua" }}>
             <CardContent style={{ display: "flex", padding: "1rem" }}>
-              <Typography variant="h5" component="div" style={{ display: "flex", width: "100%", justifyContent: "space-around"}}>
-                  <input type="text" onChange={(e) => setText(e.target.value)} placeholder="Enter Todo" />
-                  <input type="date" onChange={(e) => setDate(e.target.value)} />
-                  <button onClick={() => clickHander()}>Submit</button>
+              <Typography
+                variant="h5"
+                component="div"
+                style={{
+                  display: "flex",
+                  width: "100%",
+                  justifyContent: "space-around",
+                }}
+              >
+                <input
+                  type="text"
+                  onChange={(e) => setText(e.target.value)}
+                  placeholder="Enter Todo"
+                />
+                <input type="date" onChange={(e) => setDate(e.target.value)} />
+                <button onClick={() => clickHander()}>Submit</button>
               </Typography>
             </CardContent>
           </Card>
--- client/src/barChart.js
+++ client/src/barChart.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -7,8 +7,8 @@ import {
   Title,
   Tooltip,
   Legend,
-} from 'chart.js';
-import { Bar } from 'react-chartjs-2';
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
 
 ChartJS.register(
   CategoryScale,
@@ -20,52 +20,65 @@ ChartJS.register(
 );
 
 export default function BarChart({ chartData }) {
-    const [countArray, setCountArray] = useState([]);
-    useEffect(() => {
-        if(chartData) {
-            let mp=new Map();
-            console.log(chartData)
-            chartData.map((data) => {
-                let date = new Date(data.date).getMonth();
-                if(mp.get(date)) {
-                    mp.set(date, mp.get(date)+1);
-                } else {
-                    mp.set(date, 1);
-                }
-            })
-            let tmpArray=[];
-            for(let [key, value] of mp.entries()) {
-                tmpArray[key]=value;
-            }
-            setCountArray(tmpArray);
+  const [countArray, setCountArray] = useState([]);
+  useEffect(() => {
+    if (chartData) {
+      let mp = new Map();
+      console.log(chartData);
+      chartData.map((data) => {
+        let date = new Date(data.date).getMonth();
+        if (mp.get(date)) {
+          mp.set(date, mp.get(date) + 1);
+        } else {
+          mp.set(date, 1);
         }
-    }, [chartData]);
+      });
+      let tmpArray = [];
+      for (let [key, value] of mp.entries()) {
+        tmpArray[key] = value;
+      }
+      setCountArray(tmpArray);
+    }
+  }, [chartData]);
 
-    const data = {
-        labels,
-        datasets: [
-          {
-            label: 'Dataset 1',
-            data: countArray,
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
-          },
-        ],
-    };
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: countArray,
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
+  };
 
-    return <Bar options={options} data={data} />;
+  return <Bar options={options} data={data} />;
 }
 
 export const options = {
   responsive: true,
   plugins: {
     legend: {
-      position: 'top',
+      position: "top",
     },
     title: {
       display: true,
-      text: 'Budget Trends',
+      text: "Budget Trends",
     },
   },
 };
 
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'Noverber', 'December'];
+const labels = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "Noverber",
+  "December",
+];
